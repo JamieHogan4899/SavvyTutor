a7cb503a7534f57a2d412a60c4af3ab956ed0e9f
Jamie Hogan <20084469@mail.wit.ie>
2022-01-26 11:02:04 +0000

    Added Simple XML Layout

--- app/src/main/java/ie/wit/savvytutor/fragments/CreateAPostFragment.kt
+++ app/src/main/java/ie/wit/savvytutor/fragments/CreateAPostFragment.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
 
@@ -18,5 +20,9 @@ class CreateAPostFragment : Fragment() {
     ): View {
         //inflate the fragment layout
         return inflater.inflate(ie.wit.savvytutor.R.layout.createapost_fragment, container, false)
+
+
+
     }
+
 }
