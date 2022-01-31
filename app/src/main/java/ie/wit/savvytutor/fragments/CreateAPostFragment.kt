bd1553999675d0e63f0f9e8d0f7190403ef8a77d
Jamie Hogan <20084469@mail.wit.ie>
2022-01-31 18:33:34 +0000

    description added to model

--- app/src/main/java/ie/wit/savvytutor/fragments/CreateAPostFragment.kt
+++ app/src/main/java/ie/wit/savvytutor/fragments/CreateAPostFragment.kt
@@ -42,19 +42,22 @@ class CreateAPostFragment : Fragment() {
 
 
     fun setAddButtonListener(layout: View) {
-        //assign
+        //assign the user input fields
         val addBtn = layout.findViewById<Button>(R.id.createapostbtn)
         val title = layout.findViewById<EditText>(R.id.postTitle)
         val subject =layout.findViewById<Spinner>(R.id.chooseSubject)
         val location = layout.findViewById<EditText>(R.id.chooseLocation)
         val level = layout.findViewById<Spinner>(R.id.chooseSubject)
+        val description = layout.findViewById<EditText>(R.id.description)
 
+        //take in user input and store in the model
         addBtn.setOnClickListener {
 
             post.title = title.text.toString()
             post.subject = subject.selectedItem.toString()
             post.location = location.text.toString()
             post.level = level.selectedItem.toString()
+            post.description = description.text.toString()
 
         println(post)
             Toast.makeText(getActivity(), "Post Created" , Toast.LENGTH_LONG).show();
--- app/src/main/java/ie/wit/savvytutor/models/PostModel.kt
+++ app/src/main/java/ie/wit/savvytutor/models/PostModel.kt
@@ -1,10 +1,14 @@
 package ie.wit.savvytutor.models
 
+import java.io.FileDescriptor
+
 
 data class PostModel(var title: String = "",
                      var subject: String = "",
                      var location: String = "",
-                     var level: String = ""
+                     var level: String = "",
+                     var description: String =  ""
+
 
 
 
