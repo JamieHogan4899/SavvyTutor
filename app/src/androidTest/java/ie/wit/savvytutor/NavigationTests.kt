fb0825e805dc97dc9f8d742566ef9256256b27a9
Jamie Hogan <20084469@mail.wit.ie>
2022-02-21 13:50:39 +0000

    Using nav view test bug fixed, also added new screen to test

--- app/src/androidTest/java/ie/wit/savvytutor/NavigationTests.kt
+++ app/src/androidTest/java/ie/wit/savvytutor/NavigationTests.kt
@@ -4,13 +4,13 @@ import android.view.Gravity
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.espresso.contrib.DrawerActions
 import androidx.test.espresso.contrib.DrawerMatchers.isClosed
+import androidx.test.espresso.contrib.DrawerMatchers.isOpen
 import androidx.test.espresso.contrib.NavigationViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import ie.wit.savvytutor.activity.MainActivity
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -34,13 +34,39 @@ class NavigationTests {
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.createAPost)) //click into create a post
         onView(withId(R.id.createapost_fragment)).check(matches(isDisplayed())) //check does it display correct fragment
 
+       TimeUnit.SECONDS.sleep(3)
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open()) //open nav drawer
-        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.chat)) //click into chat
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.aboutSavvyTutor)) //click into create a post
+        onView(withId(R.id.about_fragment)).check(matches(isDisplayed())) //check does it display correct fragment
+
+        TimeUnit.SECONDS.sleep(3)
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open()) //open nav drawer
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.chat)) //click into create a post
         onView(withId(R.id.chat_fragment)).check(matches(isDisplayed())) //check does it display correct fragment
 
-        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open()) //open nav drawer
-        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.aboutSavvyTutor)) //click into chat
-        onView(withId(R.id.about_fragment)).check(matches(isDisplayed())) //check does it display correct fragment
+        TimeUnit.SECONDS.sleep(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
