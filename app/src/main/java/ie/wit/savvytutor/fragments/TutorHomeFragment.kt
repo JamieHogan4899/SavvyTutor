ce174b052092f91bc7a016dafbac64f0fe3e238c
Jamie Hogan <20084469@mail.wit.ie>
2022-04-18 17:49:14 +0100

    updated navigation view

--- app/src/main/java/ie/wit/savvytutor/fragments/TutorHomeFragment.kt
+++ app/src/main/java/ie/wit/savvytutor/fragments/TutorHomeFragment.kt
@@ -6,11 +6,13 @@ import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.view.*
+import android.widget.TextView
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import ie.wit.savvytutor.activity.MainActivity
 import ie.wit.savvytutor.models.PostModel
@@ -20,13 +22,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 private lateinit var dbRef: DatabaseReference
 private lateinit var  postRecyclerView: RecyclerView
 private lateinit var  postArrayList : ArrayList<PostModel>
-
+private lateinit var mAuth: FirebaseAuth
+var userEmail : String = ""
 
 
 class TutorHomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mAuth = FirebaseAuth.getInstance()
     }
 
     override fun onCreateView(
@@ -42,13 +46,17 @@ class TutorHomeFragment : Fragment() {
 
         postArrayList = arrayListOf<PostModel>()
         getParentPosts()
+        userEmail= mAuth.currentUser?.email.toString()
+        println(userEmail)
+        context?.let { updateNavView(it) }
+
 
 
         return root
 
     }
 
-    override fun onAttach(context: Context) {
+    fun updateNavView(context: Context) {
         super.onAttach(context)
         val activity: Activity = context as MainActivity
         val navigationView = activity.findViewById(ie.wit.savvytutor.R.id.nav_view) as NavigationView
@@ -57,7 +65,10 @@ class TutorHomeFragment : Fragment() {
         navigationView.menu.findItem(ie.wit.savvytutor.R.id.chat).isVisible = false
         navigationView.menu.findItem(ie.wit.savvytutor.R.id.aboutSavvyTutor).isVisible = false
 
-
+        val navigationHeader = activity.findViewById(ie.wit.savvytutor.R.id.nav_view) as NavigationView
+        val txtProfileName =
+            navigationHeader.getHeaderView(0).findViewById<View>(ie.wit.savvytutor.R.id.DisplayName) as TextView
+        txtProfileName.setText(userEmail)
     }
 
     private fun getParentPosts(){
