300837a75d54cbdc3493d25a7e59024fbd73cedc
Jamie Hogan <20084469@mail.wit.ie>
2022-04-22 21:31:29 +0100

    parent loading in post to CommentFragment

--- app/src/main/java/ie/wit/savvytutor/fragments/CommentFragment.kt
+++ app/src/main/java/ie/wit/savvytutor/fragments/CommentFragment.kt
@@ -4,8 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
+import ie.wit.savvytutor.R
+import ie.wit.savvytutor.models.UserModel
+import kotlinx.android.synthetic.main.comment_fragment.*
 
 var RuserId: String = ""
 var RpostTitle: String = ""
@@ -15,8 +23,18 @@ var RpostLevel: String = ""
 var RpostDescription: String = ""
 var RpostId: String = ""
 var RposterEmail: String = ""
+var posterProfilePicture = ""
+private lateinit var dbRef: DatabaseReference
+private lateinit var mAuth: FirebaseAuth
 
 class CommentFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        dbRef = FirebaseDatabase.getInstance("https://savvytutor-ab3d2-default-rtdb.europe-west1.firebasedatabase.app/").reference
+        mAuth = FirebaseAuth.getInstance()
+    }
+
     @Nullable
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +43,7 @@ class CommentFragment : Fragment() {
     ): View {
         //inflate the fragment layout
         val root = inflater.inflate(ie.wit.savvytutor.R.layout.comment_fragment, container, false)
+        getProfilePicture(root)
 
 
 
@@ -40,8 +59,52 @@ class CommentFragment : Fragment() {
         RpostId =  bundle?.getString("postId").toString()
         RposterEmail =  bundle?.getString("posterEmail").toString()
 
+        root.findViewById<TextView>(R.id.commentTitle).setText(RpostTitle)
+        root.findViewById<TextView>(R.id.commentSubject).setText(RpostSubject)
+        root.findViewById<TextView>(R.id.commentLocation).setText(RpostLocation)
+        root.findViewById<TextView>(R.id.commentLevel).setText(RpostLevel)
+        root.findViewById<TextView>(R.id.commentDescription).setText(RpostDescription)
+        root.findViewById<TextView>(R.id.commentPosterId).setText(RposterEmail)
+
+
+
         return root
     }
 
+    fun getProfilePicture(layout:View) {
+        //go to database and get the posters profile picture
+        //go the database and get all the users
+        dbRef.child("Users").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    for (userSnapshot in snapshot.children) {
+                        val users = userSnapshot.getValue(UserModel::class.java)
+                       // println(users)
+
+                        //check does any uid match the posters id
+                        if(RuserId.equals(users?.uid)){
+                            if (users != null) {
+                                //if they do get there profile picture url
+                                posterProfilePicture = users.profilepic
+                                val imageview = layout.findViewById<ImageView>(R.id.CommentPosterProfilePicutre)
+                                //println("Users Picture: " + posterProfilePicture)
+                                Picasso.get().load(posterProfilePicture.toString()).into(imageview);
+
+                            }
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+
+    }
+
+
 
 }
+
